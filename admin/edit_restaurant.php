40e7e4c31256c7b0498e97833ecc87fd933b7f2b
markov alex <markovalex2004@gmail.com>
2023-06-24 00:13:03 +0300

    Function update image

--- admin/edit_restaurant.php
+++ admin/edit_restaurant.php
@@ -55,53 +55,65 @@ else{
 	}
 	elseif (isset($_POST["updateImageRestId"])){
 		$id = $_POST["updateImageRestId"];
-		echo "update image"."<br>";
+		// echo "update image"."<br>";
 		$image = $_FILES["image"];
 		$sql = "SELECT * FROM `restaurants` WHERE `id` = $id";
 		$result = $db->query($sql);
 		if($result->num_rows > 0){
-			echo "id found"."<br>";
-			$old_image_id = $result->fetch_assoc()["image_id"];
-			$new_image_id = $old_image_id;
-			if(isImageValid($image)){
-				echo "name is valid"."<br>";
-				if(deleteImageById($new_image_id, $db)){
-					echo "deleted"."<br>";
-					$filename = uniqid() . '_' . $image['name'];
-					$remoteDir = '../uploaded_images/';
-					$remotePath = $remoteDir.$filename;
-					if (move_uploaded_file($image['tmp_name'], $remotePath)) {
-						if($new_image_id != 0){
-							echo "updated image"."<br>";
-							$sql = "UPDATE `images` SET `image_name`='$filename' WHERE `id` = $new_image_id";
-							$result = $db->query($sql);
-						}
-						else{
-							$sql = "DELETE FROM `images` WHERE id=$old_image_id";
-							$result = $db->query($sql);
-							echo "deleted old image from db"."<br>";
-							$sql = "INSERT INTO `images`(`image_name`) VALUES('$filename')";
-							$result = $db->query($sql);
-							$image_id = $db->insert_id;
-							echo "inserted new image"."<br>";
-							$sql = "UPDATE `restaurants` SET `image_id`=$image_id WHERE id=$id";
-							$result = $db->query($sql);
-							echo "updated id of image"."<br>";
-						}
-
-
-					}
-					else{
-						$errorText = "Старый файл был удален, но новый не удалось загрузить";
-					}
+			// echo "id found"."<br>";
+			$image_id = $result->fetch_assoc()["image_id"];
+			if(updateImage($image_id, $db, $image, $errorText)){
+				$sql = "UPDATE `restaurants` SET `image_id`=$image_id WHERE id=$id";
+				echo "image updated"."<br>";
+				if($db->query($sql)){
+					echo "success"."<br>";
+					header("location: restaurant_page.php?id=$id");
+					exit;
 				}
 				else{
-					$errorText = "Ошибка обновления изображения";
+					$errorText .= " Ошибка внесения изменений";
 				}
 			}
-			else{
-				$errorText = "Ошибка загрузки файла. Он должен иметь размер не больше 2 Мб";
-			}
+//			$new_image_id = $old_image_id;
+//			if(isImageValid($image)){
+//				echo "name is valid"."<br>";
+//				if(deleteImageById($new_image_id, $db)){
+//					echo "deleted"."<br>";
+//					$filename = uniqid() . '_' . $image['name'];
+//					$remoteDir = '../uploaded_images/';
+//					$remotePath = $remoteDir.$filename;
+//					if (move_uploaded_file($image['tmp_name'], $remotePath)) {
+//						if($new_image_id != 0){
+//							echo "updated image"."<br>";
+//							$sql = "UPDATE `images` SET `image_name`='$filename' WHERE `id` = $new_image_id";
+//							$result = $db->query($sql);
+//						}
+//						else{
+//							$sql = "DELETE FROM `images` WHERE id=$old_image_id";
+//							$result = $db->query($sql);
+//							echo "deleted old image from db"."<br>";
+//							$sql = "INSERT INTO `images`(`image_name`) VALUES('$filename')";
+//							$result = $db->query($sql);
+//							$image_id = $db->insert_id;
+//							echo "inserted new image"."<br>";
+//							$sql = "UPDATE `restaurants` SET `image_id`=$image_id WHERE id=$id";
+//							$result = $db->query($sql);
+//							echo "updated id of image"."<br>";
+//						}
+//
+//
+//					}
+//					else{
+//						$errorText = "Старый файл был удален, но новый не удалось загрузить";
+//					}
+//				}
+//				else{
+//					$errorText = "Ошибка обновления изображения";
+//				}
+//			}
+//			else{
+//				$errorText = "Ошибка загрузки файла. Он должен иметь размер не больше 2 Мб";
+//			}
 		}
 	}
 	try {
--- admin/functions.php
+++ admin/functions.php
@@ -33,14 +33,14 @@ function addImage(&$image, &$db, &$image_error, &$insertedImageId){
 			}
 			else{
 				$insertedImageId = $db->insert_id;
-				echo 'File uploaded successfully.';
+				// echo 'File uploaded successfully.';
 			}
 		} else {
-			echo 'Error uploading file.';
+			// echo 'Error uploading file.';
 			$image_error = true;
 		}
 	} else {
-		echo 'Ошибка в типе файла. Или размер файла больше 2 Мб';
+		// echo 'Ошибка в типе файла. Или размер файла больше 2 Мб';
 		$image_error = true;
 	}
 }
@@ -51,18 +51,76 @@ function deleteImageById(&$id, &$db){
 		$image_name = $result->fetch_assoc()["image_name"];
 		$filePath = "../uploaded_images/".$image_name;
 		if ($image_name != "no_image.jpg" && file_exists($filePath)) {
-			echo "previous file is exsist in folder:".$filePath."<br>";
+			// echo "previous file is exsist in folder:".$filePath."<br>";
 			chmod($filePath, 0644);
 			if(!unlink($filePath)){
 				return false;
 			}
 		}
+//		else{
+//			$id = 0;
+//		}
+	}
+//	else{
+//		$id = 0;
+//	}
+	return true;
+}
+
+function updateImage(&$id, &$db, &$image, &$errorText){ // returns true if success
+	// echo "id before alchemy = $id"."<br>";
+	if(isImageValid($image)){
+		// echo "new image is valid"."<br>";
+		if(deleteImageById($id, $db)){
+			// echo "old image deleted from server"."<br>";
+			$filename = uniqid() . '_' . $image['name'];
+			$remoteDir = '../uploaded_images/';
+			$remotePath = $remoteDir.$filename;
+			if (move_uploaded_file($image['tmp_name'], $remotePath)) {
+				$sql = "SELECT * FROM `images` WHERE `id`=$id";
+				$result =  $db->query($sql);
+				if($result->num_rows <= 0){
+					$id = 0;
+				}
+				// echo "id after alchemy = $id"."<br>";
+				// echo "filename = $filename"."<br>";
+				if($id == 0){
+					// echo "deleted old image from db"."<br>";
+					$sql = "INSERT INTO `images`(`image_name`) VALUES ('$filename')";
+					if($db->query($sql)){
+						$id = $db->insert_id;
+						// echo "Inserted new image to db with id $id"."<br>";
+						return true;
+					}
+					else{
+						$errorText = "Ошибка добавления изображения в базу данных, но на сервер оно было загружено";
+						return false;
+					}
+				}
+				else { // images has been found
+					$sql = "UPDATE `images` SET `image_name`='$filename' WHERE `id` = $id";
+					if($db->query($sql)){
+						// echo "updated image"."<br>";
+						return true;
+					}
+					else{
+						$errorText = "Не удалось обновить изображение";
+						return false;
+					}
+				}
+			}
+			else{
+				$errorText = "Ошибка загрузки изображения на сервер";
+				return false;
+			}
+		}
 		else{
-			$id = 0;
+			$errorText = "Ошибка удаления старого изображения";
+			return false;
 		}
 	}
 	else{
-		$id = 0;
+		$errorText = "Новое изображение не прошло валидацию. Размер файла должен быть не больше 2 Мб";
+		return false;
 	}
-	return true;
 }
