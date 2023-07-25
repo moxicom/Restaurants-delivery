bdfcf0b44fb6bcd640ba09909f0fc995179587e7
Alexander Markov <45567992+moxicom@users.noreply.github.com>
2023-07-25 13:39:54 +0300

    users orders done

--- controllers/cart_page.php
+++ controllers/cart_page.php
@@ -16,7 +16,9 @@ $view = new Environment($loader);
  */
 $dishes = array();
 $cart = array();
+$ordersArray = array();
 $user_logged = false;
+
 
 
 if(isset($_SESSION["cart"])){
@@ -32,6 +34,29 @@ if(isset($_SESSION["cart"])){
 if (isset($_SESSION["user_id"])) {
     # make request to get all orders
     $user_logged = true;
+    $user_id = $_SESSION["user_id"];
+    $sql = "SELECT orders.order_id, menu.dishName, orders.dish_amount, orders.taken 
+    FROM `orders` JOIN `menu` ON orders.dish_id = menu.id WHERE orders.user_id = $user_id";
+    if($result = $db->query($sql)){
+        while ($row = $result->fetch_assoc()) {
+            $order_id = $row['order_id'];
+
+            // Проверяем, существует ли уже массив для данного заказа
+            if (!isset($ordersArray[$order_id])) {
+                $ordersArray[$order_id] = array();
+            }
+
+            // Добавляем информацию о блюде в массив заказа
+            $orderItem = array(
+                'dish_amount' => $row['dish_amount'],
+                'taken' => $row['taken'],
+                'dishName' => $row['dishName']
+            );
+
+            $ordersArray[$order_id][] = $orderItem;
+        }
+        print_r($ordersArray);
+    }
 }
 
 try {
@@ -41,6 +66,7 @@ try {
 		"cart" => $dishes,
 		"amount" => $cart,
         "userLogged" => $user_logged,
+        "orders" => $ordersArray,
 	));
 } catch (\Twig\Error\LoaderError|\Twig\Error\RuntimeError|\Twig\Error\SyntaxError $e) {
 }
